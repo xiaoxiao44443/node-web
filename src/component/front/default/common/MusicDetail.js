81883d0376134f044b80e9850030eb521cee4b89
xiaoxiao44443 <bierqi@163.com>
2018-02-20 18:37:57 +0800

    music_wedgit player process btn border style

--- src/component/front/default/common/MusicDetail.js
+++ src/component/front/default/common/MusicDetail.js
@@ -71,7 +71,7 @@ class MusicDetail extends Component {
             touchStart: {},
             listBoxVisible: false,
             paused: true,
-            btnX: -8,
+            btnX: -5,
             currentTime: 0,
             duration: -1,
             mode: 0, // 0:列表循环 1:随机播放 2:单曲循环,
