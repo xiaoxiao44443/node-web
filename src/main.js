d92181913ac4decb382ec794360c33ae0e24c79a
xiaoxiao44443 <bierqi@163.com>
2018-03-30 03:31:10 +0800

    landing fix

--- src/main.js
+++ src/main.js
@@ -67,7 +67,7 @@ class Root extends Component {
             window.onload = () => {
                 const $el = window.document.getElementsByTagName('body')[0];
                 $el.className = 'landing';
-                setTimeout(() => $el.className = "", 250);
+                setTimeout(() => $el.className = "", 1500);
             }
         }else{
             window.document.getElementsByTagName('body')[0].className = 'landing';
